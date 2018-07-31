7e02b3947c13ad024ae5054413ae79d9060f6989
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-07-31 17:00:48 +0530

    Code Beautified

--- router/users.js
+++ router/users.js
@@ -5,103 +5,75 @@ const passport = require('passport')
 
 let User = require('../models/user');
 
-
 //Register Form
 
-router.get('/register' , (req,res) => {
+router.get('/register', (req, res) => {
 
-
-res.render('register')
+  res.render('register')
 
 });
 
-
 // Loging form
-router.get('/login' , (req,res) => {
-res.render('login')
+router.get('/login', (req, res) => {
+  res.render('login')
 
 })
 
 // Login Process Execution
 
-router.post('/login' , (req,res,next)=>{
+router.post('/login', (req, res, next) => {
 
-passport.authenticate('local' , {
-  successRedirect: '/home',
-  failureRedirect: '/users/login',
-  failureFlash : true
-})(req,res,next);
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
 
 })
 
-// Add song Router
+// Add User Route
 
-router.post('/add', (req,res) => {
+router.post('/add', (req, res) => {
 
   let newUser = new User();
 
-newUser.name = req.body.name;
-newUser.email = req.body.email;
-newUser.username = req.body.username;
-newUser.password = req.body.password;
+  newUser.name = req.body.name;
+  newUser.email = req.body.email;
+  newUser.username = req.body.username;
+  newUser.password = req.body.password;
 
-// req.checkBody('name' , 'Name is required').notEmpty();
-// req.checkBody('email' , 'Email is required').notEmpty();
-// req.checkBody('email' , 'Email is not valid').notEmpty();
-// req.checkBody('username' , 'Username is required').notEmpty();
-// req.checkBody('password' , 'Password is required').notEmpty();
+  bcrypt.genSalt(10, (err, salt) => {
 
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
 
-// let errors = req.validationErrors();
-//
-// if(errors){
-//   res.render('register'), {
-//     errors : errors
-//   }
-// }else{
-//   let newUser = new User({
-//     name : name,
-//     email : email,
-//     username : username,
-//     password : password
-//   })
-// }
-
-bcrypt.genSalt(10, (err,salt) => {
-
-  bcrypt.hash(newUser.password, salt , (err, hash) => {
-
-    if(err) {
-      console.log(err)
-    }
-    newUser.password = hash ;
-    // Password converted to Hash
-
-    // Ready to save to Database
-    newUser.save( function (err)  {
-      if(err){
-        console.log(err);
-      }else {
-        req.flash('success' , 'User Registered')
-        res.redirect('login')
+      if (err) {
+        console.log(err)
       }
-    })
+      newUser.password = hash;
+      // Password converted to Hash
 
-  });
+      // Ready to save to Database
+      newUser.save(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          req.flash('success', 'User Registered')
+          res.redirect('login')
+        }
+      })
+
+    });
+  })
+
 })
-
-})
-
 
 // Logout Route
 
-router.get('/logout' , (req,res)=> {
-
+router.get('/logout', (req, res) => {
 
   req.logout();
   res.redirect('login')
 
 });
 
-
-module.exports = router ;
+module.exports = router;
